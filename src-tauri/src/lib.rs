b0e65a01cb7423d5fbd8a3ba19509c8d864f8151
Kris <KrisShin5014@outlook.com>
2025-08-01 10:50:43 +0800

    feat: android call api done.

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,4 +1,8 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+// use tauri::Runtime;
+
+mod handle_music;
+
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
@@ -8,7 +12,12 @@ fn greet(name: &str) -> String {
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![
+            greet,
+            handle_music::search_music,
+            handle_music::download_song,
+            handle_music::get_play_url
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
